4cfb45a616fd21840cc261e6937f9ca220008c2c
thosakwe <thosakwe@gmail.com>
2016-12-29 21:04:10 -0500

    Need to fix center

--- example/web/widgets/app.dart
+++ example/web/widgets/app.dart
@@ -1,4 +1,5 @@
 import 'package:mariposa/mariposa.dart';
+import 'package:mariposa/ui.dart';
 import 'package:mariposa_material/mariposa_material.dart';
 import 'package:mariposa_material/mdl.dart';
 import 'badge_example.dart';
@@ -11,6 +12,8 @@ class App extends Widget {
 
   @override
   Node render() {
+    return new Center(child: h('h1', {}, [text('FUCK YEAH!!!')]));
+
     return new Layout()
       ..add(new PageContent()
         ..add(h('div', {'style': 'padding: 1em;'}, [new BadgeExample()])));
